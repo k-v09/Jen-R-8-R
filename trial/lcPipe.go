4ea3560aa103369310003d5f23051f5001adbb61
Noah Sehman <noah.sehman@gmail.com>
2024-09-22 13:14:59 -0700

    keys and relationships... you'll see *cool guy emoji*

--- trial/lcPipe.go
+++ trial/lcPipe.go
@@ -8,6 +8,38 @@ import (
 	"strings"
 )
 
+var (
+	octave    = 4
+	relations = map[string]int{
+		"c":  -9,
+		"c#": -8,
+		"d":  -7,
+		"d#": -6,
+		"e":  -5,
+		"f":  -4,
+		"f#": -3,
+		"g":  -2,
+		"g#": -1,
+		"a":  0,
+		"a#": 1,
+		"b":  2,
+	}
+	keys = map[string]string{
+		"z": "c",
+		"s": "c#",
+		"x": "d",
+		"d": "d#",
+		"c": "e",
+		"v": "f",
+		"g": "f#",
+		"b": "g",
+		"h": "g#",
+		"n": "a",
+		"j": "a#",
+		"m": "b",
+	}
+)
+
 func main() {
 	pipePath := "/tmp/pipe_frequency"
 	for {
@@ -27,6 +59,8 @@ func main() {
 			if strings.HasPrefix(line, "p:") {
 				key := strings.TrimPrefix(line, "p:")
 				fmt.Printf("Key pressed: %s\n", key)
+				fmt.Printf("Playing note: %s\n", keys[key])
+				fmt.Printf("Distance from A: %s\n", string(relations[keys[key]]))
 			} else if strings.HasPrefix(line, "r:") {
 				key := strings.TrimPrefix(line, "r:")
 				fmt.Printf("Key released: %s\n", key)
